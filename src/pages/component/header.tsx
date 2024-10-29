07a97f53f5999dca53c0b59cdac12133ff417f40
Boris-sud0 <cheng.boris@hotmail.com>
2024-10-29 01:35:50 +0100

    [ADD] Header + les réseaux sociaux

--- src/pages/component/header.tsx
+++ src/pages/component/header.tsx
@@ -3,13 +3,17 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <section>
-            <div>
-                <Image src="/logo boris.png" alt="logo portfolio" width={500} height={300} />
+        <section className={Style.header}>
+            <div style={{marginLeft: "20px"}}>
+                <Image src="/logo boris.png" alt="logo portfolio" width={40} height={40} />
             </div>
-            <div>
-                <p> hello </p>
-            </div>
+                <div > Accueil </div>
+                <div> Présentation</div>
+                <div> Mes projets</div>
+                <div> Obejectif Futur</div>
+                <div> Mes compétences</div>
+                <div>Contacter moi</div>
+                <br></br>
         </section>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,32 @@
 import styles from '../styles/index.module.css'
 import Header from './component/header';
+import Image from 'next/image';
 
 export default function home() {
   return (
     <>
       <Header />
-        <section className={styles.wallpaper}>
-          <div className={styles.picture}></div>
+      <section className={styles.wallpaper}>
+        <div className={styles.picture}></div>
+        <div>
           <h1 className={styles.title}>Bienvenue, Voici mon portfolio</h1>
-          <div className={styles.reseau}> Github : </div>
+        </div>
+        <div>
+          <span>Github</span>
+          <a href="https://github.com/Baollu" target="_blank" rel="noopener noreferrer">
+            <Image src="/github_image.jpg" alt="logo github" width={40} height={40} />
+          </a>
+        </div>
+        <div>
+          <span>Linkedin</span>
+          <a href='https://www.linkedin.com/in/boris-cheng-8010992a1/' target='blank' rel='noopener noreferrer'>
+            <Image src='/linkedin.png' alt="logo linkedin" width={40} height={40} />
+          </a>
+        </div>
+        <section className={styles.portail}>
+          <p>Présentation</p>
         </section>
+      </section>
     </>
   );
 }
