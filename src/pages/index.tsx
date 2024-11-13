7b810352d96c0e0e1433ff332c77d4678a0c8784
Baollu <boris.cheng@epitech.eu>
2024-11-13 18:19:30 +0100

    position homepage picture and tittle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,23 +6,25 @@ import Link from 'next/link';
 export default function home() {
   return (
     <>
-      <Header />
+
       <section className={styles.wallpaper}>
-        <div className={styles.picture}></div>
-        <div>
+        <div className={styles.headerContent}>
+          <div className={styles.picture}></div>
           <h1 className={styles.title}>Bienvenue, Voici mon portfolio</h1>
         </div>
-        <div>
-          <span>Github</span>
-          <a href="https://github.com/Baollu" target="_blank" rel="noopener noreferrer">
-            <Image src="/github_image.jpg" alt="logo github" width={40} height={40} />
-          </a>
-        </div>
-        <div>
-          <span>Linkedin</span>
-          <a href='https://www.linkedin.com/in/boris-cheng-8010992a1/' target='blank' rel='noopener noreferrer'>
-            <Image src='/linkedin.png' alt="logo linkedin" width={40} height={40} />
-          </a>
+        <div className={styles.navLinks}>
+          <div>
+            <span>Github</span>
+            <a href="https://github.com/Baollu" target="_blank" rel="noopener noreferrer">
+              <Image src="/github_image.jpg" alt="logo github" width={40} height={40} />
+            </a>
+          </div>
+          <div>
+            <span>Linkedin</span>
+            <a href="https://www.linkedin.com/in/boris-cheng-8010992a1/" target="blank" rel="noopener noreferrer">
+              <Image src="/linkedin.png" alt="logo linkedin" width={40} height={40} />
+            </a>
+          </div>
         </div>
         <div>
           <Link href="/presentation">
@@ -31,17 +33,17 @@ export default function home() {
         </div>
         <div>
           <Link href="project">
-            <p> Mes projets</p>
+            <p>Mes projets</p>
           </Link>
         </div>
         <section>
           <Link href="competence">
-            <p> Mes compétences</p>
+            <p>Mes compétences</p>
           </Link>
         </section>
         <section>
           <Link href="futur">
-            <p> Mes Objectif</p>
+            <p>Mes Objectifs</p>
           </Link>
         </section>
       </section>
